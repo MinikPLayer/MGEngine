9398a1876124b5d39483f7bb2ee3b0d58cfda225
Michał Tomecki <minexplaypl@gmail.com>
2025-12-04 22:57:45 +0100

    Added lib-src to avoid the need for external libraries. Fixed Windows compilation.

--- UserScript/main.cpp
+++ UserScript/main.cpp
@@ -1,12 +1,6 @@
 #include <Engine.hpp>
-#include <GameObject.hpp>
-#include <Log.hpp>
-#include <Mesh.hpp>
-#include <TimeUtils.hpp>
 
-#include "Camera.hpp"
 #include "TestObject.hpp"
-#include "Renderer/IRenderer.hpp"
 
 int main() {
 	Engine::init();
